8961ba61f5e7fefd0fa68e8d6ab55b941eeeb0b9
Anjasfedo <fedoaf98@gmail.com>
2024-05-13 00:09:58 +0700

    Do// Form Templates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 1 }
+    validates :content, presence: true, length: { minimum: 5, maximum: 10 }
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_12_164215) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_12_170824) do
   create_table "documents", force: :cascade do |t|
     t.string "title"
     t.string "content"
