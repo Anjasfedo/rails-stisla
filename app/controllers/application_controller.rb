02b32346b6d3ccf0eb2fc245bbb347917ab5f605
Anjasfedo <fedoaf98@gmail.com>
2024-05-16 18:01:21 +0700

    Do// sweet Alert 2 for Delete Confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  before_action :set_login_time
+
+  private
+
+  def set_login_time
+    session[:login_time] ||= Time.current if current_user
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 
+import Swal from "sweetalert2";
+window.Swal = Swal;
