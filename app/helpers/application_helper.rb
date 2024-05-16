9faf2491a8e1d4e72e2360fa09400d097f2395d6
Anjasfedo <fedoaf98@gmail.com>
2024-05-16 17:05:49 +0700

    Do// Login & Logout Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def devise_controller?
+    controller_name == 'sessions' || controller_name == 'registrations'
+  end
+
+  def devise_action?
+    (controller_name == 'sessions' && action_name == 'new') ||
+      (controller_name == 'registrations' && (action_name == 'new' || action_name == 'create'))
+  end
 end
