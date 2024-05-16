a1f8dd25972af0ad8a72456ed608cd8d756ccf94
Anjasfedo <fedoaf98@gmail.com>
2024-05-16 18:59:14 +0700

    Do// User Edit Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,12 @@ module ApplicationHelper
     controller_name == 'sessions' || controller_name == 'registrations'
   end
 
-  def devise_action?
+  def devise_auth_action?
     (controller_name == 'sessions' && action_name == 'new') ||
       (controller_name == 'registrations' && (action_name == 'new' || action_name == 'create'))
+  end
+
+  def devise_update_profile?
+    controller_name == 'registrations' && (action_name == 'edit' || action_name == 'update')
   end
 end
